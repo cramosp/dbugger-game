c0dc8988bd9f9a3544d4cc35e16edf25a5424f2a
Cristina RP <cristinarp95@gmail.com>
2023-10-30 13:35:58 +0100

    changed to px

--- js/main.js
+++ js/main.js
@@ -1,48 +1,36 @@
 const myBoard = document.getElementById("board");
 const myBoardBounds = myBoard.getBoundingClientRect();
 const myBoardStyle = window.getComputedStyle(myBoard);
-// const myBoardBorderWidth = parseInt(myBoardStyle.borderWidth, 10);
-const boardBorderWidthPixels = parseInt(myBoardStyle.borderWidth, 10);
-const boardBorderWidthVW = (boardBorderWidthPixels / myBoardBounds.width) * 100; // Calculate the border width in vw
-
-const myBoardBorderWidth = boardBorderWidthVW;
-// const myBoardWidth = myBoardBounds.width - myBoardBorderWidth * 2;
-const viewportWidth = window.innerWidth;
-const boardWidthPixels = myBoardBounds.width - myBoardBorderWidth * 2;
-const boardWidthVW = (boardWidthPixels / viewportWidth) * 100;
-
-const myBoardWidth = boardWidthVW;
+const myBoardBorderWidth = parseInt(myBoardStyle.borderWidth, 10);
+const myBoardWidth = myBoardBounds.width - myBoardBorderWidth * 2;
 console.log(myBoardWidth);
 
 class Player {
   constructor() {
-    this.height = 10;
-    this.width = 5;
+    this.height = 70;
+    this.width = 40;
     this.positionX = 0; // Fix it.
     this.positionY = 0;
-    this.step = 2; // It can move faster by changing this.
-    console.log(this.positionX);
+    this.step = 15;
 
-    // display it in UI:
     this.playerElm = document.getElementById("player");
-    this.playerElm.style.width = this.width + "vw";
-    this.playerElm.style.height = this.height + "vh";
-    this.playerElm.style.left = this.positionX + "vw";
-    this.playerElm.style.bottom = this.positionY + "vh";
+    this.playerElm.style.width = this.width + "px";
+    this.playerElm.style.height = this.height + "px";
+    this.playerElm.style.left = this.positionX + "px";
+    this.playerElm.style.bottom = this.positionY + "px";
   }
 
   moveLeft() {
     if (this.positionX > 0) {
       this.positionX -= this.step;
-      this.playerElm.style.left = this.positionX + "vw";
+      this.playerElm.style.left = this.positionX + "px";
     }
   }
   moveRight() {
-    if (this.positionX < myBoardWidth - myBoardBorderWidth - this.width) {
+    if (this.positionX < myBoardWidth - this.width) {
       this.positionX += this.step;
-      this.playerElm.style.left = this.positionX + "vw";
+      this.playerElm.style.left = this.positionX + "px";
     }
-    console.log(this.positionX);
   }
 }
 
